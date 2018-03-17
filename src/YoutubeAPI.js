33a8c996ef0eb58abe102b0b57ae899108990c79
Caue Queiroz <cauenqueiroz@gmail.com>
2018-03-17 16:52:29 -0300

    Generating a simple json from the requested json

--- src/YoutubeAPI.js
+++ src/YoutubeAPI.js
@@ -1,18 +1,25 @@
 import HttpRequest from './HttpRequest';
 
 export default class YoutubeAPI {
-  constructor(options) {
-    if (options === undefined || options.apiKey === undefined) {
-      throw new Error('You need to pass an `apiKey` to the class constructor.');
-    }
-
-    this.apiKey = options.apiKey;
+  constructor({ apiKey }) {
+    this.apiKey = apiKey;
     this.apiUrl = 'https://www.googleapis.com/youtube/v3';
   }
 
   getSearchResultsFor(query) {
     const url = `${this.apiUrl}/search?part=snippet&key=${this.apiKey}&q=${query}`;
 
-    return HttpRequest.to(url);
+    const rawToVideo = raw => ({
+      id: raw.id.videoId,
+      title: raw.snippet.title,
+      description: raw.snippet.description,
+      thumbnail: raw.snippet.thumbnails.high.url
+    });
+
+    return HttpRequest
+      .to(url)
+      .then(result => result.items)
+      .then(items => items.map(item => rawToVideo(item)))
+      .then(videos => ({ videos }));
   }
 }
--- test/YoutubeAPI.spec.js
+++ test/YoutubeAPI.spec.js
@@ -3,7 +3,9 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
 import fetch from 'node-fetch';
+
 import YoutubeAPI from '../src/YoutubeAPI';
+import rawJSON from '../mock/paramore';
 
 /*  Initial setup
 ----------------------------------------------------------------------------------------- */
@@ -17,13 +19,13 @@ global.fetch = fetch;
 
 describe('YoutubeAPI', () => {
 
+  let youtube;
+
+  beforeEach(() => {
+    youtube = new YoutubeAPI({ apiKey: 'foo' });
+  });
+
   describe('Smoke Tests', () => {
-
-    let youtube;
-
-    beforeEach(() => {
-      youtube = new YoutubeAPI({ apiKey: 'foo' });
-    });
 
     it('should create an instance of YoutubeAPI', () => {
       expect(youtube).to.be.an.instanceof(YoutubeAPI);
@@ -34,7 +36,7 @@ describe('YoutubeAPI', () => {
     });
 
     it('should throw an error when there is no `apiKey`', () => {
-      expect(() => new YoutubeAPI()).to.throw('You need to pass an `apiKey` to the class constructor.');
+      expect(() => new YoutubeAPI()).to.throw("Cannot read property 'apiKey' of undefined");
     });
 
     it('should have a method `getSearchResultsFor`', () => {
@@ -47,12 +49,10 @@ describe('YoutubeAPI', () => {
 
     let stubedFetch;
     let promise;
-    let youtube;
 
     beforeEach(() => {
       stubedFetch = sinon.stub(global, 'fetch');
       promise = stubedFetch.returnsPromise();
-      youtube = new YoutubeAPI({ apiKey: 'foo' });
     });
 
     afterEach(() => {
@@ -67,6 +67,30 @@ describe('YoutubeAPI', () => {
     it('should call the fetch function with the right url', () => {
       youtube.getSearchResultsFor('Paramore');
       expect(stubedFetch).to.have.been.calledWith('https://www.googleapis.com/youtube/v3/search?part=snippet&key=foo&q=Paramore');
+    });
+
+    it('should return a simple json from the raw json', () => {
+      promise.resolves({ json: () => rawJSON });
+
+      const finalJSON = {
+        "videos": [
+          {
+            "id": "cW5ueE2vUm0",
+            "title": "Paramore: Rose-Colored Boy [OFFICIAL VIDEO]",
+            "description": "We're doing it LIVE! Get tickets to The After Laughter Summer Tour with Foster The People at http://paramore.net/tour Paramore's music video for 'Rose-Colored Boy' from the album, After Laughter...",
+            "thumbnail": "https://i.ytimg.com/vi/cW5ueE2vUm0/hqdefault.jpg"
+          },
+          {
+            "id": "AEB6ibtdPZc",
+            "title": "Paramore: Hard Times [OFFICIAL VIDEO]",
+            "description": "We're doing it LIVE! Get tickets to The After Laughter Summer Tour with Foster The People at http://paramore.net/tour Paramore's music video for 'Hard Times' from the album, After Laughter...",
+            "thumbnail": "https://i.ytimg.com/vi/AEB6ibtdPZc/hqdefault.jpg"
+          }
+        ]
+      };
+
+      const request = youtube.getSearchResultsFor('Paramore');
+      expect(request.resolveValue).to.be.eql(finalJSON);
     });
 
   });
